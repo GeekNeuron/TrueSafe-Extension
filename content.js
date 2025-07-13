49bc10f970c148f7ae35221dde514447473921fb
Geek Neuron <geekneuron@gmail.com>
2025-07-13 10:13:59 +0330

    Update content.js

--- content.js
+++ content.js
@@ -1,4 +1,10 @@
-// This script checks for the presence of a password field on the page.
+const MAX_TEXT_LENGTH = 20000;
+
+if (document.body && document.body.innerText) {
+    const pageText = document.body.innerText.substring(0, MAX_TEXT_LENGTH);
+    chrome.runtime.sendMessage({ type: "PAGE_CONTENT_FOR_HASHING", text: pageText });
+}
+
 if (document.querySelector('input[type="password"]')) {
   chrome.runtime.sendMessage({ type: "PASSWORD_FIELD_DETECTED" });
 }
